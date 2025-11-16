5f3df699f591d7cea915f78479999d0ce7dfca06
He Xing <77628887+Eric6669@users.noreply.github.com>
2025-11-16 22:18:51 +0800

    Update gt.py

--- SolMuseum/dae/gt.py
+++ SolMuseum/dae/gt.py
@@ -44,9 +44,10 @@ class gt:
         self.TFS = kwargs.get('TFS')
         self.Tref = kwargs.get('Tref')
         self.c = kwargs.get('c')
+        self.kNL = kwargs.get('kNL')
 
         self.Tmec = None
-        self.kNL = None
+        # self.kNL = None
         self.qT_i = None
         self.qT = None
         # self.Tref = None
@@ -68,7 +69,7 @@ class gt:
         self.qf = self.Cop
         self.xv = self.qf
         self.qfuel = self.xv
-        self.qR = self.W * (self.wref - self.syn.omega)
+        self.qR = (self.qfuel - self.kNL) / (1 - self.kNL)
         self.Te = self.TRbase + self.D * (1 - self.qf) + self.E * (1 - self.syn.omega)
         self.Tri = self.K2 * self.Te
         self.Tr = self.K1 * self.Te + self.Tri
@@ -77,9 +78,6 @@ class gt:
             self.Tref = self.Tx
         self.qT = self.qR
         self.qT_i = (self.qT - self.kp * (self.Tref - self.Tx)) / self.ki
-
-        q = np.minimum(self.qT, np.clip(self.qR, self.qmin, self.qmax))
-        self.kNL = (q - self.qfuel) / (q - 1)
 
         self.phi = self.c * self.syn.Pm
 
@@ -149,7 +147,7 @@ class gt:
         # fuel
         m.kNL = Param('kNL_' + name, self.kNL)
         m.qfuel = Var('qfuel_' + name, self.qfuel)
-        rhs = m.qfuel - (m.kNL + (1 - m.kNL) * Min(m.qT, Saturation(m.qR, m.qmin, m.qmax)) * m.omega)
+        rhs = m.qfuel - (m.kNL + (1 - m.kNL) * Min(m.qT, Saturation(m.qR, m.qmin, m.qmax)))
         m.FuelCons = Eqn('Fuel Consumption', rhs)
 
         # Valve Positioner
